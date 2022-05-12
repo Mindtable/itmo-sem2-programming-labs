065228af03564eefac2a9363a0acb79a526eda6a
Mindtable <mtble@mail.ru>
2022-05-12 21:57:40 +0300

    refactor: lab6

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -7,14 +7,6 @@
 
 int main(int argc, char *argv[]) {
     // 4x^2 + 3x + 10, point 20
-    using ff = qq<
-            value<10>,
-            poly0<factor<9>, deg<3>>,
-            poly0<factor<3>, deg<2>>,
-            poly0<factor<2>, deg<1>>,
-            poly0<factor<1>, deg<0>>
-            >;
-    std::cout << ff::val << std::endl;
     return RUN_ALL_TESTS();
 }
 
